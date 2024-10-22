0c29ea521f67ab64de5cbd8259a0753f7e8ab0a5
Roman Rudolf <roman_rudolf@icloud.com>
2024-10-22 21:51:22 +0100

    register works, but make email unique now

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,26 @@ const User = require('../models/User');
 const { registerValidation } = require('../validations/validation');
 
 router.post('/register', async (req, res) => {
-    res.send(registerValidation(req.body));
+
+    const { error } = registerValidation(req.body);
+    if (error) {
+        res.send({ message: error['details'][0]['message'] });
+    }
+
+    const newUser = new User({
+        user_name: req.body.user_name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try{
+        // .save() words on mongoDB mongoose object
+        const savedUser = await newUser.save();
+        res.send(savedUser);
+    } catch(err) {
+        res.status(400).send({ message: err });
+    }
+    
 });
 
 router.post('/login', async (req, res) => {
